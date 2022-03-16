318d8083b1f3612208764d4045a26cd2f07a4f5d
Harun Doğdu <harundogdu06@gmail.com>
2022-03-16 15:14:54 +0300

    generate dummy data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,35 @@
-import {LoaderFunction, useLoaderData} from "remix";
+import {json, LoaderFunction, useLoaderData} from "remix";
 import Main from "~/components/main/main";
-
+import {Post} from "~/types/post";
 
 export const loader: LoaderFunction = () => {
     const posts = [
         {
+            id: 1,
             title: 'Hello World',
             content: 'This is my first blog post!',
+            slug: 'hello-world',
         },
         {
+            id: 2,
             title: 'Second Post',
             content: 'This is my second blog post!',
+            slug: 'second-post',
         },
+        {
+            id: 3,
+            title: 'Third Post',
+            content: 'This is my third blog post!',
+            slug: 'third-post',
+        }
     ]
 
-    return {
-        posts,
-    }
-
+    return json(posts);
 }
 
-
 export default function Index() {
-    const post = useLoaderData();
+    const posts = useLoaderData<Post[]>();
     return (
-        <div>
-           <Main />
-        </div>
+        <Main posts={posts}/>
     );
 }
